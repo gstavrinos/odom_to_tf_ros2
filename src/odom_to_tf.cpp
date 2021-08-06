315a2660112b670542be1348cb6332aae6b779e7
George Stavrinos <stavrinosgeo@gmail.com>
2021-08-06 08:40:57 +0300

    Added the ability to change the odom frame_id and child_frame_id

--- src/odom_to_tf.cpp
+++ src/odom_to_tf.cpp
@@ -11,18 +11,35 @@ class OdomToTF : public rclcpp::Node {
     public:
         OdomToTF() : Node("odom_to_tf") {
             std::string odom_topic;
-            this->get_parameter_or("odom_topic", odom_topic, std::string("/odom/perfect"));
+            frame_id = this->declare_parameter("frame_id", std::string(""));
+            child_frame_id = this->declare_parameter("child_frame_id", std::string(""));
+            odom_topic = this->declare_parameter("odom_topic", std::string("/odom/perfect"));
+            RCLCPP_INFO(this->get_logger(), "odom_topic set to %s", odom_topic.c_str());
+            if (frame_id != "") {
+                RCLCPP_INFO(this->get_logger(), "frame_id set to %s", frame_id.c_str());
+            }
+            else {
+                RCLCPP_INFO(this->get_logger(), "frame_id was not set. The frame_id of the odom message will be used.");
+            }
+            if (child_frame_id != "") {
+                RCLCPP_INFO(this->get_logger(), "child_frame_id set to %s", child_frame_id.c_str());
+            }
+            else {
+                RCLCPP_INFO(this->get_logger(), "child_frame_id was not set. The child_frame_id of the odom message will be used.");
+            }
             sub_ = this->create_subscription<nav_msgs::msg::Odometry>(odom_topic, rclcpp::SensorDataQoS(), std::bind(&OdomToTF::odomCallback, this, _1));
             tfb_ = std::make_shared<tf2_ros::TransformBroadcaster>(this);
         }
     private:
-        rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr sub_;
+        std::string frame_id, child_frame_id;
         std::shared_ptr<tf2_ros::TransformBroadcaster> tfb_;
+        rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr sub_;
         void odomCallback(const nav_msgs::msg::Odometry::SharedPtr msg) const {
 
             geometry_msgs::msg::TransformStamped tfs_;
             tfs_.header = msg->header;
-            tfs_.child_frame_id = msg->child_frame_id;
+            tfs_.header.frame_id = frame_id != "" ? frame_id : tfs_.header.frame_id;
+            tfs_.child_frame_id = child_frame_id != "" ? child_frame_id : msg->child_frame_id;
             tfs_.transform.translation.x = msg->pose.pose.position.x;
             tfs_.transform.translation.y = msg->pose.pose.position.y;
             tfs_.transform.translation.z = msg->pose.pose.position.z;
